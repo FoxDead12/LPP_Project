b179c3c405052343d6472f2691bf136b32f4f293
psychosykes <josepedrodiazmoreira@gmail.com>
2023-06-14 23:44:34 +0100

    só falta o form do create product

--- src/home/products/Product.java
+++ src/home/products/Product.java
@@ -29,7 +29,7 @@ public class Product extends javax.swing.JPanel {
 
         getAllProducts();
 
-        saveChangesButton.setEnabled(false);
+        save_changes.setEnabled(false);
     }
 
 
@@ -43,9 +43,9 @@ public class Product extends javax.swing.JPanel {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        addButton = new javax.swing.JButton();
-        saveChangesButton = new javax.swing.JButton();
-        deleteButton = new javax.swing.JButton();
+        AddButton = new javax.swing.JButton();
+        save_changes = new javax.swing.JButton();
+        delete = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 255, 255));
 
@@ -58,14 +58,14 @@ public class Product extends javax.swing.JPanel {
 
             },
             new String [] {
-                "Name", "Price", "Quantity"
+                "Nome", "Preço", "Quantidade"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Double.class, java.lang.Integer.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, true, true
+                false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -83,34 +83,34 @@ public class Product extends javax.swing.JPanel {
         jTable1.setSelectionBackground(new java.awt.Color(102, 153, 255));
         jScrollPane1.setViewportView(jTable1);
 
-        addButton.setBackground(new java.awt.Color(25, 118, 240));
-        addButton.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        addButton.setForeground(new java.awt.Color(255, 255, 255));
-        addButton.setText("Add Product");
-        addButton.setBorder(null);
-        addButton.addActionListener(new java.awt.event.ActionListener() {
+        AddButton.setBackground(new java.awt.Color(25, 118, 240));
+        AddButton.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        AddButton.setForeground(new java.awt.Color(255, 255, 255));
+        AddButton.setText("Adicionar Produto");
+        AddButton.setBorder(null);
+        AddButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                addButtonActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
 
-        saveChangesButton.setBackground(new java.awt.Color(25, 118, 240));
-        saveChangesButton.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        saveChangesButton.setForeground(new java.awt.Color(255, 255, 255));
-        saveChangesButton.setText("Save Changes");
-        saveChangesButton.setBorder(null);
-        saveChangesButton.addActionListener(new java.awt.event.ActionListener() {
+        save_changes.setBackground(new java.awt.Color(25, 118, 240));
+        save_changes.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        save_changes.setForeground(new java.awt.Color(255, 255, 255));
+        save_changes.setText("Guardar");
+        save_changes.setBorder(null);
+        save_changes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                saveChangesButtonActionPerformed(evt);
+                save_changesActionPerformed(evt);
             }
         });
 
-        deleteButton.setBackground(new java.awt.Color(255, 0, 51));
-        deleteButton.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        deleteButton.setForeground(new java.awt.Color(255, 255, 255));
-        deleteButton.setText("Delete");
-        deleteButton.setBorder(null);
-        deleteButton.addActionListener(new java.awt.event.ActionListener() {
+        delete.setBackground(new java.awt.Color(255, 0, 51));
+        delete.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        delete.setForeground(new java.awt.Color(255, 255, 255));
+        delete.setText("Apagar");
+        delete.setBorder(null);
+        delete.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 deleteActionPerformed(evt);
             }
@@ -120,14 +120,16 @@ public class Product extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1184, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(deleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(saveChangesButton, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(AddButton, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 542, Short.MAX_VALUE)
+                        .addComponent(delete, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(save_changes, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1160, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -135,25 +137,25 @@ public class Product extends javax.swing.JPanel {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(saveChangesButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(deleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                    .addComponent(AddButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(save_changes, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(delete, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 595, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
     
-    private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
         Create create = new Create(this);
         create.setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }                                        
 
-    private void saveChangesButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveChangesButtonActionPerformed
+    private void save_changesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveChangesButtonActionPerformed
 
         // Save updates
         try {
-            saveChangesButton.setEnabled(false);
+            save_changes.setEnabled(false);
             ArrayList<String> valuesQuery = new ArrayList<>();
             for (RowToUpdate row : rowToUpdate) {
 
@@ -173,7 +175,7 @@ public class Product extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_saveChangesButtonActionPerformed
 
-    private void deleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteActionPerformed
+    private void deleteActionPerformed(java.awt.event.ActionEvent evt) {                                       
         
         if (jTable1.getSelectedRows().length == 0) {
             return;
@@ -193,7 +195,7 @@ public class Product extends javax.swing.JPanel {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }//GEN-LAST:event_deleteButtonActionPerformed
+    }                                            
 
     private void getAllProducts() {
         productList.clear();
@@ -224,7 +226,7 @@ public class Product extends javax.swing.JPanel {
                     // Here goes your code "on cell update"
                     rowToUpdate.add(new RowToUpdate(evt.getFirstRow(), productList.get(evt.getFirstRow()).getId()));
                     if (!rowToUpdate.isEmpty()) {
-                        saveChangesButton.setEnabled(true);
+                        save_changes.setEnabled(true);
                     }
                 }
             }
@@ -249,11 +251,11 @@ public class Product extends javax.swing.JPanel {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton addButton;
-    private javax.swing.JButton deleteButton;
+    private javax.swing.JButton AddButton;
+    private javax.swing.JButton delete;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JButton saveChangesButton;
+    private javax.swing.JButton save_changes;
     // End of variables declaration//GEN-END:variables
 
 
